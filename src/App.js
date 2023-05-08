1e11b5ef6f26f20f9b683065d0fc2f813f739d86
Patryk Grudzień <patryk.grudzien@chargify.com>
2023-05-08 11:23:43 +0200

    fix: 🐛 Render the main App.js

--- src/App.js
+++ src/App.js
@@ -19,8 +19,6 @@ export default function App() {
 
         componentsFactory.create('subscriptions-manager').render('#root');
     }, []);
-  return (
-      <div id={'root'}/>
-  );
+    return null;
 }
 
